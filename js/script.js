5b1da6f816d5bd9e4d08ea9491bb834a23ca7686
lukaszplac123 <lukasz.plac@gmail.com>
2017-08-21 22:25:12 +0200

    Tree corrected

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 function drawTree(nodes) {
-  for(var i = 0; i < nodes; i++){
+  for(var i = 1; i <= nodes; i++){
     star = "";
     gap = "";
-    for(var j = 0; j <= i; j++){
+    for(var j = 0; j < i * 2 - 1; j++){
       star += "*";
     };
     for(var k = nodes - i; k > 0; k--){
       gap += " ";
     };
-    console.log(gap + star + star);
+    console.log(gap + star);
   };
 }
+
 drawTree(5);
